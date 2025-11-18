d632c54a7ba10eba61ce597da005ed36c80e21f1
Tatiana Tsogoeva <tsogoevats@gmail.com>
2025-11-18 21:55:35 +0700

    add structure and svg for who section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import '../scss/style.scss';
-import './hero-bg.js';
+import { animatePoints } from './hero-bg.js';
+import { animatePins } from './pins.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.classList.add('loaded')
+    document.body.classList.add('loaded');
+    animatePoints();
+    animatePins(["pin1", "pin2", "pin3", "pin4"], 1000);
 });
